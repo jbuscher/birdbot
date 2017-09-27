5fd11e0b517089f21f649848a39e7756c989621f
John Buscher <jbuscher@uw.edu>
2017-09-26 18:26:30 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,69 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+var giphy = require('giphy-api')('zRB0k85tUVrcWd6RMLFo82vlelGRWAuS');
+
+var birdWords = ["birb", "partyparrot", "birdswitharms", "crow", "owl", "bird", "parrot", "birdperson"];
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0].toLowerCase();
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !bird
+            case 'bird':
+                getGif(userID, birdWords, function(url) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: url
+                    });
+                });
+            break;
+            // !hots
+            case 'hots':
+                getGif('', ['bat signal'], function(url) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: url
+                    });
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
+
+function getGif(user, searchTermArray, callback) {
+    var index = Math.floor(Math.random() * searchTermArray.length);
+    var dickbutt = user == "234532950039986186"
+    var dickbuttVariation = Math.floor(Math.random() * 2);
+    var searchTerm;
+    if (dickbutt && dickbuttVariation == 1) {
+        searchTerm = "dickbutt";
+    } else {
+        searchTerm = searchTermArray[index];
+    }
+    giphy.random(searchTerm, function (err, res) {
+        var url = res.data.url;
+        callback(url);
+    });
+}
